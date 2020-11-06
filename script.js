7187cfcfcf5371f4ed1715abe5149958b33a2220
Aleksandr Shin <axel.shin@gmail.com>
2020-11-06 16:08:22 +0300

    accordeon done

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var tabs_title = document.querySelectorAll('.tabs-title a'),
     tabs_body = document.querySelectorAll('.tabs-body');
 tabs_title.forEach(function(elem,index){
-    elem.addEventListener('click', function(e){      
-        e.preventDefault;
+    elem.addEventListener('click', function(e){              
         if( !e.target.classList.contains('active') ){
             tabs_title.forEach(function(elem2,index2){
                 if( index === index2 ){
@@ -20,6 +19,19 @@ tabs_title.forEach(function(elem,index){
                     elem3.classList.remove('active');
                 }                
             });            
-        }        
+        }
+        return false
     });
 })
+
+var accordeon = document.querySelectorAll('.accordeon-item');
+accordeon.forEach(function(elem){
+    elem.addEventListener('click', function(e){
+        if( e.target.classList.contains('active') ){
+            e.target.classList.remove('active')
+        }
+        else{
+            e.target.classList.add('active')
+        }
+    })
+});
