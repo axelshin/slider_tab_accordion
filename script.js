f2af7c55c6688fc28340286b3cb67000df1f104b
Aleksandr Shin <axel.shin@gmail.com>
2020-11-06 17:59:19 +0300

    slider done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*******tabs*******/
 var tabs_title = document.querySelectorAll('.tabs-title a'),
     tabs_body = document.querySelectorAll('.tabs-body');
 tabs_title.forEach(function(elem,index){
@@ -23,7 +24,8 @@ tabs_title.forEach(function(elem,index){
         return false
     });
 })
-
+/*******tabs_end*******/
+/*******accordeon*******/
 var accordeon = document.querySelectorAll('.accordeon-item');
 accordeon.forEach(function(elem){
     elem.addEventListener('click', function(e){
@@ -35,3 +37,64 @@ accordeon.forEach(function(elem){
         }
     })
 });
+/*******accordeon_end*******/
+/*******slider*******/
+
+var sliderItems = document.querySelectorAll('.slider-item'),
+    sliderItemsWrap = document.querySelector('.slider-inner'),
+    sliderTrack = document.querySelector('.slider-track'),
+    sliderItemWidth = parseFloat(getComputedStyle( sliderItemsWrap , null).width.replace("px", "")),
+    sliderImtesAmount = sliderItems.length,
+    sliderNewItems = sliderTrack.innerHTML + sliderTrack.innerHTML + sliderTrack.innerHTML,
+    index = 0,
+    sliderPrev = document.querySelector('.slider-prev'),
+    sliderNext = document.querySelector('.slider-next'),
+    iint = false;
+
+    sliderTrack.innerHTML = sliderNewItems;
+    sliderTrack.style.transform = 'translateX(' + -sliderImtesAmount*sliderItemWidth + 'px)';    
+    function prev() {        
+        if( iint ){
+            return
+        }        
+        iint = true;
+        sliderTrack.classList.add('transition');
+        index--;
+        sliderTrack.style.transform = 'translateX('+ -1 * sliderItemWidth * (sliderImtesAmount + index) + 'px)';
+        if( index == -sliderImtesAmount ){
+            setTimeout(function(){
+                index = 0;
+                sliderTrack.classList.remove('transition');
+                sliderTrack.style.transform = 'translateX(' + -sliderImtesAmount*sliderItemWidth + 'px)';
+            }, 500)
+        }
+        setTimeout(function(){
+            iint = false;
+        }, 500)
+    }
+
+    function next() {
+        if( iint ){
+            return
+        }        
+        iint = true;
+        sliderTrack.classList.add('transition');
+        index++
+        sliderTrack.style.transform = 'translateX('+ -1 * sliderItemWidth * (sliderImtesAmount + index) + 'px)';
+
+        if( index == sliderImtesAmount ){
+            setTimeout(function(){
+                index = 0;
+                sliderTrack.classList.remove('transition');
+                sliderTrack.style.transform = 'translateX(' + -sliderImtesAmount*sliderItemWidth + 'px)';
+            }, 500)
+        }
+        setTimeout(function(){
+            iint = false;
+        }, 500)
+    }
+    
+    sliderPrev.addEventListener('click', prev);
+    sliderNext.addEventListener('click', next);
+
+/*******slider_end*******/
